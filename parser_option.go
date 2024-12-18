2dbd6f9d64a8fd0ae97ae12194e24c37c708d9b9
cyberxnomad <uuidxx@163.com>
2024-12-18 15:58:36 +0800

    feat(parser): add comments for parser options

--- parser_option.go
+++ parser_option.go
@@ -4,12 +4,14 @@ import "time"
 
 type ParserOption func(*Parser)
 
+// WithLayout allows to specify custom layout.
 func WithLayout(layout []LayoutField) ParserOption {
 	return func(p *Parser) {
 		p.layout = layout
 	}
 }
 
+// WithDefaultLocation allows to specify default location if no location specified in expression.
 func WithDefaultLocation(location *time.Location) ParserOption {
 	return func(p *Parser) {
 		p.defaultLoction = location
